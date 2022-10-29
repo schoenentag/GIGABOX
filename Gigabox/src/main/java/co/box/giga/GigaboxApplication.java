b2fd03cab2468f987b8798207ff145632fd617a4
nxbetr <nxbetr@gmail.com>
2022-10-29 10:12:42 +0900

    부트적용

---
+++ Gigabox/src/main/java/co/box/giga/GigaboxApplication.java
@@ -0,0 +1,14 @@
+package co.box.giga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigaboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigaboxApplication.class, args);
+	}
+	
+	
+}
---
+++ Gigabox/src/test/java/co/box/giga/GigaboxApplicationTests.java
@@ -0,0 +1,13 @@
+package co.box.giga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigaboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
