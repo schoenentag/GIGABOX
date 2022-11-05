e75ac26472a195cadcda37cd97da58a3c6caf5fc
han <siny9713@gmail.com>
2022-11-05 21:52:03 +0900

    20221105 메인, 상세페이지 작업중

--- Gigabox/src/main/java/co/box/giga/movie/controller/MovieController.java
+++ Gigabox/src/main/java/co/box/giga/movie/controller/MovieController.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import co.box.giga.movie.service.MovieAPI;
 import co.box.giga.movie.service.MovieService;
 
 
@@ -16,12 +16,17 @@ public class MovieController {
 	
 	
 	@GetMapping("/movieMain.do")
-	public String movieMain(Model model) {
-		
-		//MovieAPI api = new MovieAPI();
-		//api.requestAPI();
+	public String movieMain() {
 		
 		return "movie/movieMain";
+	}
+	@GetMapping("/movieListOne.do")
+	public String movieListOne(Model model, @RequestParam("key") String key, @RequestParam("movieCd") int movieCd){
+		
+		model.addAttribute("key", key);
+		model.addAttribute("movieCd", movieCd);
+		
+		return "movie/movieListOne";
 	}
 
 }
